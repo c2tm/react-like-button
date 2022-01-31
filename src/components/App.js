861c78b1cb249b993bd70a180dae514d80677888
c2tm <cyork4839@gmail.com>
2022-01-31 15:47:22 -0500

    recent commit

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import {useState} from 'react';
+import './App.css';
+
+
+
+function App() {
+
+  const [buttonCount, countUpdate] = useState(0);
+
+  const buttonText = () => {
+    if (buttonCount === 1) {
+      return (`1 Like`);
+    } else {
+      return (`${buttonCount} Likes`);
+    }
+  }  
+
+
+  return (
+    <div className="App">
+      <button type="submit" className="button" onClick={() => countUpdate(buttonCount + 1)}>
+        <span>{buttonText()}</span>                           
+        </button>
+    </div>
+  );
+}
+
+export default App;
